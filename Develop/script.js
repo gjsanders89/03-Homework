4d5efec7066694e3e59b2ad351748c5755e6e27e
Garrett Sanders <gjsanders.89@gmail.com>
2022-03-31 21:56:29 -0500

    skeleton and small bits of work

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var charLength = 0;
+// the variable "charNum" is a number value generated as the result of the charNum function below
 
 // Write password to the #password input
 function writePassword() {
@@ -10,5 +12,31 @@ function writePassword() {
 
 }
 
+function setCharNum() {
+
+}
+
+function generatePassword() {
+
+}
+
+
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+/* 
+
+VARIABLES
+  var to hold each password criteria
+    > length (text field)
+    > types of characters (drop down menu)
+  var to hold the charLength 
+  var to hold the charType
+
+
+FUNCTIONS
+  function to handle button press and to reveal prompts for password criteria
+
+*/
